e3080677f93474bca159b3febf57d9a90f7b4d61
anon-0110101 <alexanderhart21@criptext.com>
2023-07-10 12:02:25 -0600

    3 Testing Contracts

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -13,3 +13,15 @@ main().catch((error) => {
     console.error(error);
     process.exitCode = 1;
   });
+
+
+  //created a token
+  //put it on the blockchain
+  //opened the console
+  //interacted with the token
+  //created solidity source code
+  //fetched the solidity created source code
+  //learned how to write tests
+  //basic things with ERC-20 tokens
+  //cleaned up code
+  
---
+++ test/Token.js
@@ -0,0 +1,53 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const tokens = (n) => {
+    return ethers.utils.parseUnits(n.toString(), "ether")
+}
+
+describe("Token", () =>{
+    //Tests go inside here
+    let token 
+    
+    beforeEach(async () => {
+        //Code that gets executed before each one of these examples (below)
+        //fetch Token from blockchain
+        const Token = await ethers.getContractFactory("Token")
+        token = await Token.deploy("Rancho Las Monjas", "RLM", "1000000")
+
+    })
+
+    describe('Deployment', () => {
+
+        const name = "Rancho Las Monjas";
+        const symbol = "RLM";
+        const decimals = "18";
+        const totalSupply = tokens('1000000');
+        it("Token has correct name", async () => {
+            //read token name
+            //check that name is correct
+            expect(await token.name()).to.equal(name)
+    
+        })
+    
+        it("Token has correct symbol", async () => {
+            //read token symbol
+            //check that symbol is correct
+            expect(await token.symbol()).to.equal(symbol)
+    
+        })
+        
+        it("Token has correct decimals", async () => {
+            //read token symbol
+            //check that symbol is correct
+            expect(await token.decimals()).to.equal(decimals)
+    
+        })
+    
+        it("Token has correct total Supply", async () => {
+            //read token symbol
+            //check that symbol is correct
+            expect(await token.totalSupply()).to.equal(totalSupply)
+        })
+    })
+})
